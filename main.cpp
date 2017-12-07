ae962b5db6f56712ca371a2aed9fbb85ae5364e2
Faaa97 <faaa97@hotmail.com>
2017-12-07 13:08:21 +0000

    Prueba erode-dilate2

--- main.cpp
+++ main.cpp
@@ -79,6 +79,7 @@ int main(int argc, char** argv)
                 // CODIGO 2.1
                 // limpiar la máscara del fondo de ruido
                 //...
+		int dilation_size = 5;
 		Mat element = getStructuringElement(MORPH_RECT,Size(2*dilation_size+1),Point(dilation_size,dilation_size));
 		erode(frame,frame,element);
 		dilate(frame,frame,element);
